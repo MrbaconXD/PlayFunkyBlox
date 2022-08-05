eca67047ccca2b9eaafa29437282c532579128e7
BaconXD <97074586+MrbaconXD@users.noreply.github.com>
2022-08-05 15:43:20 +0700

    Update autoplayer.lua

--- autoplayer.lua
+++ autoplayer.lua
@@ -1,15 +1,6 @@
 --Its just a UI and Autoplayer so just ingore it
+wait(2)
 pcall(function()
 loadstring(game:HttpGet("https://raw.githubusercontent.com/MrbaconXD/GUI/main/GUI.lua"), true)()
 end)
-
--- Just message
-wait(3)
-local b = BrickColor.new("Red")
-game.StarterGui:SetCore("ChatMakeSystemMessage", {
-Text = "[AutoPlayer]: Success Access";
-Font = Enum.Font.Cartoon;
-Color = b.Color;
-FontSize = Enum.FontSize.Size96;
-})
 -- Dont worry its will always work on all executor and not bug or error
