c8a11e5b1cd3e19a997e1d8e92b08a42d3408d72
BaconXD <97074586+MrbaconXD@users.noreply.github.com>
2022-08-05 16:57:48 +0700

    Update autoplayer.lua
    Lets go hacking time brrrrr

--- autoplayer.lua
+++ autoplayer.lua
@@ -1,6 +1,6 @@
 --Its just a UI and Autoplayer so just ingore it
-wait(2)
+wait(2) -- slow down due to alot of bytes
 pcall(function()
-loadstring(game:HttpGet("https://raw.githubusercontent.com/MrbaconXD/GUI/main/GUI.lua"), true)()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/MrbaconXD/GUI/main/ExecGUI.lua"), true)()
 end)
 -- Dont worry its will always work on all executor and not bug or error
